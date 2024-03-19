80462e1fd8fbd1829c98a250f84084586a31b4a2
Gavrilo Aleksic <galeksic@apple.com>
2024-03-19 09:17:36 +0100

    add routing parser

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,16 @@
 const http = require("http");
 const routes = require("./routes");
+const { mapToRoute, logRequest } = require("./utils");
 
 http
   .createServer((req, res) => {
     const { url, method } = req;
+    const router = mapToRoute(url, method, routes);
+    if (!router) {
+      res.writeHead(404);
+      res.end();
+      return;
+    }
+    logRequest({ router, url });
   })
   .listen(3000);
--- nodejs/utils.js
+++ nodejs/utils.js
@@ -1,11 +1,39 @@
+/**
+ * Maps the url to the route handler
+ * @constructor
+ * @param {string} url - url to be processed
+ * @param {string} method -  HTTP Method (GET/POST/PUT/DELETE)
+ * @param {Record<string,{method: string, handler: () => void}>} routes
+ */
 const mapToRoute = (url, method, routes) => {
-  return Object.keys(routes).find((r) => {
-    const urlParts = url.split("/");
-    const routesPart = routes.split("/");
-    if (urlParts.length !== routesPart.length) return false;
+  let foundRoute = null;
+  Object.keys(routes).find((r) => {
+    if (routes[r].method !== method || foundRoute) return;
+    const urlParts = url.split("/").filter((e) => !!e);
+    const routesParts = r.split("/").filter((e) => !!e);
+    if (urlParts.length !== routesParts.length) return;
+
+    if (
+      urlParts.every((urlPart, index) => {
+        return routesParts[index].startsWith(":")
+          ? true
+          : urlPart === routesParts[index];
+      })
+    ) {
+      foundRoute = routes[r];
+    }
   });
+  return foundRoute;
 };
+
+const logRequest = ({ router, url }) =>
+  console.log(
+    `[${new Date().toISOString()}] Route: ${router.route} Method: ${
+      router.method
+    }, URL: ${url}`
+  );
 
 module.exports = {
   mapToRoute,
+  logRequest,
 };
