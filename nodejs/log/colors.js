a0351502f442dddf40d5865d2649f99ba19d1e47
Gavrilo Aleksic <53422814+gavrilo-aleksic@users.noreply.github.com>
2024-03-27 13:42:14 +0100

    Add coloring logs to nodejs app

---
+++ nodejs/log/colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  black: "30m",
+  red: "31m",
+  green: "32m",
+  yellow: "33m",
+  blue: "34m",
+  magenta: "35m",
+  cyan: "36m",
+  white: "37m",
+};
+
+module.exports = { Colors };
--- nodejs/log/log.js
+++ nodejs/log/log.js
@@ -1,9 +1,28 @@
-const logRequest = ({ router, url }) =>
+const { Colors } = require("./colors");
+
+const colorText = (text, color = Colors.white) => `\x1b[${color}${text}\x1b[0m`;
+
+const currentTimestamp = `${colorText(new Date().toISOString(), Colors.cyan)}`;
+
+const logRequest = ({ router, url }) => {
+  if (!router) {
+    return console.log(
+      `[${currentTimestamp}] URL: ${colorText(url, Colors.magenta)} ${colorText(
+        "Not Found",
+        Colors.red
+      )}`
+    );
+  }
   console.log(
-    `[${new Date().toISOString()}] Route: ${router?.path} Method: ${
-      router?.method
-    }, URL: ${url}`
+    `[${currentTimestamp}] Route: ${colorText(
+      router?.path,
+      router?.path !== undefined ? Colors.blue : Colors.red
+    )} Method: ${colorText(
+      router?.method,
+      router?.method ? Colors.yellow : Colors.red
+    )}, URL: ${colorText(url, Colors.magenta)}`
   );
+};
 
 module.exports = {
   logRequest,
