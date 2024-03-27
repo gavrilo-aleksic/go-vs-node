6d33b6637434b80c0e2cfcb2fbde3801d3fc3902
Gavrilo Aleksic <53422814+gavrilo-aleksic@users.noreply.github.com>
2024-03-27 15:50:40 +0100

    remove unused method in logging

--- go/log/log.go
+++ go/log/log.go
@@ -5,20 +5,18 @@ import (
 	"net/url"
 	"time"
 )
-func LogRequest(method string, url *url.URL, path string) {
+func LogRequest(url *url.URL, path string) {
 	now := time.Now()
 	if path == "" {
-		fmt.Println(fmt.Sprintf("[%s] Method: %s, URL: %s %s" ,
+		fmt.Println(fmt.Sprintf("[%s] URL: %s %s" ,
 		ColorText(now.Format("2006-01-02 15:04:05"), Black), 
-		ColorText(method, Yellow), 
 		ColorText(url.Path, Magenta),
 		ColorText("Not Found", Red),
 	))
 	} else {
-		fmt.Println(fmt.Sprintf("[%s] Route: %s Method: %s, URL: %s" ,
+		fmt.Println(fmt.Sprintf("[%s] Route: %s, URL: %s" ,
 		ColorText(now.Format("2006-01-02 15:04:05"), Black), 
 		ColorText(path, Blue),
-		ColorText(method, Yellow), 
 		ColorText(url.Path, Magenta)),
 	)
 	}
--- go/routes/routes.utils.go
+++ go/routes/routes.utils.go
@@ -9,12 +9,12 @@ import (
 func MapRoutes (mux *http.ServeMux, routes Routes) {
 	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		var response = handlers.Response{OriginalResponse:  res}
-		log.LogRequest(req.Method, req.URL, "")
+		log.LogRequest(req.URL, "")
 		response.Send(404, "Not Found")
 	})
 	for i := 0; i < len(routes.routes); i++ {
 		mux.HandleFunc(routes.routes[i].path, func(res http.ResponseWriter, req *http.Request)  {
-			log.LogRequest(req.Method, req.URL, routes.routes[i].path)
+			log.LogRequest(req.URL, routes.routes[i].path)
 			 routes.routes[i].handler(handlers.CreateRequest(req), handlers.Response{OriginalResponse: res,  }) 
 				} )
 	}
